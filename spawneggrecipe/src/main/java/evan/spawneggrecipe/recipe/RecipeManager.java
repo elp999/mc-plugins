f354f04ef6111ed4af3b202ac823dac509b83192
Evan <elp9@njit.edu>
2026-05-07 13:10:42 -0400

    adding dynamic recipe loading from a file

--- spawneggrecipe/src/main/java/evan/spawneggrecipe/recipe/RecipeManager.java
+++ spawneggrecipe/src/main/java/evan/spawneggrecipe/recipe/RecipeManager.java
@@ -1,17 +1,49 @@
 package evan.spawneggrecipe.recipe;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class RecipeManager {
 
     private final JavaPlugin plugin;
+
+    public static File[] getFiles() {
+
+        String workingDirectory = System.getProperty("user.dir");
+        File directory = new File(workingDirectory);
+
+        File[] files = directory.listFiles();
+        
+        return files;
+    }
 
     public RecipeManager(JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
     public void registerRecipes() {
-        new CreeperEggRecipe(plugin).register();
-        new EndermanEggRecipe(plugin).register();
+
+        getFiles();
+        for (File file : getFiles()) {
+            String fileName = file.getName();
+            try {
+                // Remove file extension and construct class name
+                String className = fileName.substring(0, fileName.lastIndexOf('.'));
+                String fullClassName = "evan.spawneggrecipe.recipe." + className;
+                
+                // Use reflection to instantiate the recipe class
+                Class<?> recipeClass = Class.forName(fullClassName);
+                Object recipe = recipeClass.getConstructor(JavaPlugin.class).newInstance(plugin);
+                
+                // Call register method using reflection
+                recipeClass.getMethod("register").invoke(recipe);
+            } catch (Exception e) {
+                plugin.getLogger().warning("Failed to register recipe from file: " + fileName);
+                e.printStackTrace();
+
+                // new CreeperEggRecipe(plugin).register();
+                // new EndermanEggRecipe(plugin).register();
+            }
+        }
     }
 }
