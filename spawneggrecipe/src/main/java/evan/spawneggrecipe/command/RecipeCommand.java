3cb8cda872ae4b91ff390ba00b88959bf6fef218
Evan <elp9@njit.edu>
2026-05-12 14:37:53 -0400

    testing gui recipe command

--- spawneggrecipe/src/main/java/evan/spawneggrecipe/command/RecipeCommand.java
+++ spawneggrecipe/src/main/java/evan/spawneggrecipe/command/RecipeCommand.java
@@ -1,9 +1,13 @@
 package evan.spawneggrecipe.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class RecipeCommand implements CommandExecutor {
 
@@ -17,29 +21,40 @@ public class RecipeCommand implements CommandExecutor {
 
         String type = args[0].toLowerCase();
 
+        Inventory gui = Bukkit.createInventory(null, InventoryType.WORKBENCH, "§8Custom Recipe Creator");
+                ((org.bukkit.entity.Player) sender).openInventory(gui);
+
         switch (type) {
 
             case "enderman":
-                sender.sendMessage(ChatColor.GREEN + "Enderman Spawn Egg Recipe:");
-                sender.sendMessage("R = End Rod");
-                sender.sendMessage("E = Ender Pearl");
-                sender.sendMessage("S = Egg");
-                sender.sendMessage("O = Ender Eye");
-                sender.sendMessage("");
-                sender.sendMessage("ROR");
-                sender.sendMessage("ESE");
-                sender.sendMessage("ROR");
+
+                gui.setItem(0, new ItemStack(org.bukkit.Material.END_ROD)); //block 1
+                gui.setItem(1, new ItemStack(org.bukkit.Material.ENDER_PEARL)); //block 2
+                gui.setItem(2, new ItemStack(org.bukkit.Material.EGG)); //block 3
+                gui.setItem(3, new ItemStack(org.bukkit.Material.ENDER_EYE)); //block 4
+                gui.setItem(4, new ItemStack(org.bukkit.Material.END_ROD)); //block 5
+                gui.setItem(5, new ItemStack(org.bukkit.Material.ENDER_PEARL)); //block 6
+                gui.setItem(6, new ItemStack(org.bukkit.Material.EGG)); //block 7
+                gui.setItem(7, new ItemStack(org.bukkit.Material.ENDER_EYE)); //block 8
+                gui.setItem(8, new ItemStack(org.bukkit.Material.END_ROD)); //block 9
+
+                gui.setItem(9, new ItemStack(org.bukkit.Material.ENDERMAN_SPAWN_EGG)); // crafting result
                 break;
 
             case "creeper":
-                sender.sendMessage(ChatColor.GREEN + "Creeper Spawn Egg Recipe:");
-                sender.sendMessage("T = TNT");
-                sender.sendMessage("E = Egg");
-                sender.sendMessage("G = Gunpowder");
-                sender.sendMessage("");
-                sender.sendMessage("TGT");
-                sender.sendMessage("TET");
-                sender.sendMessage("TGT");
+                
+                gui.setItem(0, new ItemStack(org.bukkit.Material.TNT)); //block 1
+                gui.setItem(1, new ItemStack(org.bukkit.Material.GUNPOWDER)); //block 2
+                gui.setItem(2, new ItemStack(org.bukkit.Material.TNT)); //block 3
+                gui.setItem(3, new ItemStack(org.bukkit.Material.TNT)); //block 4
+                gui.setItem(4, new ItemStack(org.bukkit.Material.EGG)); //block 5
+                gui.setItem(5, new ItemStack(org.bukkit.Material.TNT)); //block
+                gui.setItem(6, new ItemStack(org.bukkit.Material.TNT)); //block 7
+                gui.setItem(7, new ItemStack(org.bukkit.Material.GUNPOWDER)); //block 8
+                gui.setItem(8, new ItemStack(org.bukkit.Material.TNT)); //block 9
+
+                gui.setItem(9, new ItemStack(org.bukkit.Material.CREEPER_SPAWN_EGG)); // crafting result
+
                 break;
 
             default:
