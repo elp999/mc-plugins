702a1f8685847bd637772f797ed06fc59e2d0c62
Evan <elp9@njit.edu>
2026-05-14 14:51:49 -0400

    spawnegg recipes work + gui, need to fix bugs

--- spawneggrecipe/src/main/java/evan/spawneggrecipe/recipe/CreeperEggRecipe.java
+++ spawneggrecipe/src/main/java/evan/spawneggrecipe/recipe/CreeperEggRecipe.java
@@ -33,29 +33,6 @@ public class CreeperEggRecipe {
     }
 
     public void register() {
-
-        // Result item (Creeper Spawn Egg)
-        //ItemStack result = new ItemStack(Material.CREEPER_SPAWN_EGG);
-//
-        //// Unique recipe key
-        //NamespacedKey key = new NamespacedKey(plugin, "creeper_spawn_egg");
-//
-        //// Create shaped recipe
-        //ShapedRecipe recipe = new ShapedRecipe(key, result);
-//
-        //// Shape (3x3 crafting table)
-        //recipe.shape(
-        //        "TGT",
-        //        "TET",
-        //        "TGT"
-        //);
-//
-        //// Ingredients
-        //recipe.setIngredient('G', Material.GUNPOWDER);
-        //recipe.setIngredient('E', Material.EGG);
-        //recipe.setIngredient('T', Material.TNT);
-
-        // Register recipe
         Bukkit.addRecipe(getRecipe());
     }
 }
